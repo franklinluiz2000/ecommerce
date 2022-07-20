5b34f74e7d9efa13b5f12937eb0de00ad728f9e5
frankstark <franklinluiz2000@gmail.com>
2022-07-20 19:32:49 -0300

    criando paginas html

--- virtual_store/views.py
+++ virtual_store/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home_page(request):
-    return HttpResponse("Olá Mundo!")
+    context = {
+                    "title": "Home Page",
+                    "content": "Bem vindo a Home Page",
+              }
+    return render(request, "home/home_page.html", context)
+    
+def about_page(request):
+    context = {
+                    "title": "Página Sobre about",
+                    "content": "Bem vindo a página sobre"
+              }
+    return render(request, "about/about_page.html", context)
+
+def contact_page(request):
+    context = {
+                    "title": "Página Sobre contact",
+                    "content": "Bem vindo a página sobre"
+              }
+    return render(request, "contact/contact_page.html", context)
