bdc721c155ed16e615a21dbd1476c6d40fe83676
frankstark <franklinluiz2000@gmail.com>
2022-07-20 20:57:00 -0300

    criação do form de contato e de login

--- virtual_store/urls.py
+++ virtual_store/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page, about_page, contact_page
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 
 urlpatterns = [
     path('', home_page),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
+    path('login/', login_page, name='login'),
+    path('register', register_page, name='register'),
     path('admin/', admin.site.urls),
 ]
